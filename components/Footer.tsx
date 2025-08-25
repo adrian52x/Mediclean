5508e8fc4d145f1263c5d79db16f5a3586f05bcb
Enachi Adrian <50320663+adrian52x@users.noreply.github.com>
2025-08-25 12:25:18 +0200

    Update footer branding from MEDICLEAN to DEZINFECT

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -89,10 +89,10 @@ function Trademark() {
         <div className="mb-6 hidden md:mb-0 md:block">
             <span className="flex flex-col">
                 <h2 className="text-sm font-semibold whitespace-nowrap uppercase">
-                MEDICLEAN
+                DEZINFECT MD
                 </h2>
                 <span className="mt-2 text-sm text-neutral-500 dark:text-neutral-400">
-                © {new Date().getFullYear()} MEDICLEAN™ . All Rights Reserved.
+                © {new Date().getFullYear()} DEZINFECT™ . All Rights Reserved.
                 </span>
             </span>
         </div>
